1cb78ef0bdc94c9eb521d533771970055fb82bac
fc01 <ikinee@gmail.com>
2017-01-12 17:46:50 +0800

    01.12

--- SinaSpider/dbhelper.py
+++ SinaSpider/dbhelper.py
@@ -23,23 +23,21 @@ class dbhelper(object):
         return self.dbcursor
 
     def insertItem(self, item, tableName):
-        sql = self.getInsertSQL(tableName=tableName, item=item)
-        self.dbcursor.execute(sql, item.values())
-        self.conn.commit()
-
-    def clearTable(self, tableName, where):
-        pass
-
-    def updateItem(self, item, tableName):
-        pass
-
-    def getInsertSQL(self, item, tableName):
         placeholders = ', '.join(['%s'] * len(dict(item)))
         columns = ', '.join(item.keys())
         sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (tableName, columns, placeholders)
-        return sql
+        self.dbcursor.execute(sql, item.values())
+        self.conn.commit()
 
-    def getUpdateSQL(self, item):
+    # filter:删除记录条件参数，类型为字段和条件值组成的元组，如: {field1:value, field2:value2}
+    def deleteItem(self, tableName, filter):
+        columns = (' '.join(['%s='] * len(dict(filter))) % tuple(filter.keys())).split()  # 结果样式：[field1=, field2=, field3= ...]
+        whereClause = ' AND '.join([column + '%s' for column in columns]) % tuple(filter.values())
+        sql = "DELETE FROM %s WHERE %s" % (tableName, whereClause)
+        self.dbcursor.execute(sql)
+        self.conn.commit()
+
+    def updateItem(self, item, tableName):
         pass
 
     def __del__(self):
